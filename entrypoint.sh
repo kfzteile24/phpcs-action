ed5a71746c19d273d2c68c8871f352228512803d
Swilam <ahmed.swilam@kfzteile24.de>
2020-04-23 14:24:22 +0200

    Add bootstrap option

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,11 +7,11 @@ echo "::add-matcher::${RUNNER_TEMP}/_github_workflow/problem-matcher.json"
 if [ -z "${INPUT_ENABLE_WARNINGS}" ] || [ "${INPUT_ENABLE_WARNINGS}" = "false" ]; then
     echo "Check for warnings disabled"
 
-    ${INPUT_PHPCS_BIN_PATH} ${INPUT_DIR} --standard=${INPUT_STANDARD} -n --report=checkstyle
+    ${INPUT_PHPCS_BIN_PATH} ${INPUT_DIR} --standard=${INPUT_STANDARD} -n --report=checkstyle --bootstrap=erp-connector/vendor/sensio/distribution-bundle/Resources/bin/build_bootstrap.php
 else
     echo "Check for warnings enabled"
 
-    ${INPUT_PHPCS_BIN_PATH} ${INPUT_DIR} --standard=${INPUT_STANDARD} --report=checkstyle
+    ${INPUT_PHPCS_BIN_PATH} ${INPUT_DIR} --standard=${INPUT_STANDARD} --report=checkstyle --bootstrap=erp-connector/vendor/sensio/distribution-bundle/Resources/bin/build_bootstrap.php
 fi
 
 status=$?
