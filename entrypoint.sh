009d6353288a558605f494fc924778c56b4a8db5
Ionut Maita <ionut.maita@arnia.ro>
2020-06-16 08:43:05 +0300

    Display autoload.php

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,6 +7,9 @@ find /github/workspace/customer/vendor/kfzteile24/php-coding-standard/Kfz24 -pri
 
 echo "::add-matcher::${RUNNER_TEMP}/_github_workflow/problem-matcher.json"
 
+echo "Display autoload content"
+cat /usr/bin/phpcs/autoload.php
+
 if [ -z "${INPUT_ENABLE_WARNINGS}" ] || [ "${INPUT_ENABLE_WARNINGS}" = "false" ]; then
     echo "Check for warnings disabled"
 
@@ -14,7 +17,7 @@ if [ -z "${INPUT_ENABLE_WARNINGS}" ] || [ "${INPUT_ENABLE_WARNINGS}" = "false" ]
 else
     echo "Check for warnings enabled"
 
-    ${INPUT_PHPCS_BIN_PATH} ${INPUT_DIR} --standard=${INPUT_STANDARD} -n --report=checkstyle
+    ${INPUT_PHPCS_BIN_PATH} ${INPUT_DIR} --standard=${INPUT_STANDARD} --report=checkstyle
 fi
 
 status=$?
